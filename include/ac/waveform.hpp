7be39395d7640483b1ba2879962f4a9201d7c163
Austin Chen <austin.yhc@gmail.com>
2021-12-06 23:26:13 +0800

    :apple: Add `ac::filesystem`

--- include/ac/waveform.hpp
+++ include/ac/waveform.hpp
@@ -7,7 +7,15 @@
 #include <fstream>
 
 namespace Eigen {
-
+template<class Matrix>
+void write_binary(const char* filename, const Matrix& matrix){
+    std::ofstream out(filename, std::ios::out | std::ios::binary | std::ios::trunc);
+    typename Matrix::Index rows=matrix.rows(), cols=matrix.cols();
+    out.write((char*) (&rows), sizeof(typename Matrix::Index));
+    out.write((char*) (&cols), sizeof(typename Matrix::Index));
+    out.write((char*) matrix.data(), rows*cols*sizeof(typename Matrix::Scalar) );
+    out.close();
+}
 template<class Matrix>
 void read_binary(const char* filename, Matrix& matrix) {
     std::ifstream in(filename, std::ios::in | std::ios::binary);
