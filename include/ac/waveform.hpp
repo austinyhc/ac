3ec739dd92fad9260901cdc10889fa7d31f3a337
Austin Chen <austin.yhc@gmail.com>
2021-12-13 22:13:15 +0800

    :ocean: Add ac::Waveform

--- include/ac/waveform.hpp
+++ include/ac/waveform.hpp
@@ -1,31 +1,29 @@
 #ifndef AC_WAVEFORM_HPP
 #define AC_WAVEFORM_HPP
 
-#include <tuple>
-#include <Eigen/Dense>
-#include <iostream>
-#include <fstream>
+#include "Eigen/Dense"
+#include "NumCpp.hpp"
+#include <typeinfo>
 
-namespace Eigen {
-template<class Matrix>
-void write_binary(const char* filename, const Matrix& matrix){
-    std::ofstream out(filename, std::ios::out | std::ios::binary | std::ios::trunc);
-    typename Matrix::Index rows=matrix.rows(), cols=matrix.cols();
-    out.write((char*) (&rows), sizeof(typename Matrix::Index));
-    out.write((char*) (&cols), sizeof(typename Matrix::Index));
-    out.write((char*) matrix.data(), rows*cols*sizeof(typename Matrix::Scalar) );
-    out.close();
-}
-template<class Matrix>
-void read_binary(const char* filename, Matrix& matrix) {
-    std::ifstream in(filename, std::ios::in | std::ios::binary);
-    typename Matrix::Index rows=0, cols=0;
-    in.read((char*) (&rows),sizeof(typename Matrix::Index));
-    in.read((char*) (&cols),sizeof(typename Matrix::Index));
-    matrix.resize(rows, cols);
-    in.read( (char *) matrix.data() , rows*cols*sizeof(typename Matrix::Scalar) );
-    in.close();
-}
-} // Eigen
+namespace ac {
+class Waveform {
+public:
+    Waveform(const std::string& path) : _path(path) {
+        _signals = nc::fromfile<int16_t>(_path).astype<float>();
+        int n_samples = (int)(_signals.shape().cols / 32 / 2);
+        _signals = _signals.reshape(n_samples, -1);
+        std::cout << _signals.shape() << std::endl;
+
+        //std::cout << _signals << std::endl;
+    }
+
+    std::string path() { return _path; }
+    nc::NdArray<float> signals() { return _signals; }
+
+private:
+    std::string _path{""};
+    nc::NdArray<float> _signals;
+};
+} // ac
 
 #endif
