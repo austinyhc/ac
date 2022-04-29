3741eeba45d7cfa7375e71c632c6a03cade978e5
Austin Chen <austin.yhc@gmail.com>
2022-04-29 14:04:44 +0800

    apple

--- tests/01-utility.cpp
+++ tests/01-utility.cpp
@@ -12,5 +12,6 @@ TEST_CASE ("Factorials of 1 and higher are computed", "[single-file]") {
     REQUIRE( Factorial(1) == 1 );
     REQUIRE( Factorial(2) == 2 );
     REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(4) == 6 );
     REQUIRE( Factorial(10) == 3628800 );
 }
